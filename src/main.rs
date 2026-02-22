8c9ab1fe56ca37e01e4d3f9e811f417bf0f00cfe
Middleman <ballentineow@gmail.com>
2026-02-22 07:41:00 -0500

    Testing Beckett's personality as a TtTE&F EngineType enum; do you copy, Polaris?

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-struct TrainCar {
+/*struct TrainCar {
     id: u32,
     contents: String,
 }
+*/
+
+enum EngineType {
+    Diesel,
+    Thomas,
+    Percy,
+}
 
 fn main() {
+
+
+let beckett = EngineType::Diesel;
+let description = describe_personality(beckett);
+println!("Beckett's personality: {}", description);
+
+/*
     // 1. The car itself must be 'mut' so we can change it later
     let mut car = TrainCar {
         id: 7,
@@ -23,10 +37,22 @@ fn main() {
 
     println!("After adjustment: Car {} now contains {}", car.id, car.contents);
     println!("{} says, 'Don't nick.'", car.contents);
+    */
 }
 
+
 // This function takes a MUTABLE reference to a TrainCar
+/*
 fn fix_engine(car: &mut TrainCar) {
     // We replace the 'Diesel' clay with 'Thomas' clay
     car.contents = String::from("Thomas");
+}
+*/
+
+fn describe_personality(engine: EngineType) -> String{
+    match engine {
+        EngineType::Diesel => String::from("Diesel is a troublemaker, always causing mischief and chaos on the tracks."),
+        EngineType::Thomas => String::from("Thomas is a friendly and helpful engine, always ready to lend a hand and make friends."),
+        EngineType::Percy => String::from("Percy is a brave and intuitive little engine that doesn't always think things through, but always tries his best."),
+    }
 }
